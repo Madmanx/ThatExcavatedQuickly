1e63e254646fc3f4a9443cd65caa27063d136aaf
Unknown <remi.boscher@gmail.com>
2018-04-16 20:18:57 +0200

    Battle system and persistence try out

---
+++ Assets/Scripts/Persistence/Database.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CharacterData
+{
+    [SerializeField]
+    public string name;
+
+
+}
+
+[CreateAssetMenu(fileName = "Data", menuName = "Custom/Database", order = 1)]
+public class Database : ScriptableObject {
+
+    [SerializeField]
+    List<CharacterData> characters;
+
+
+    public void ResetAll()
+    {
+        characters = new List<CharacterData>();
+
+        // Adding colors
+        int idCharacter = 0;
+        string[] strNames = { "Jean", "Bob" };
+        characters.Add(new CharacterData { name = strNames[idCharacter] });
+        characters.Add(new CharacterData { name = strNames[++idCharacter] });
+
+    }
+}
